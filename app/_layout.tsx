48057f1a2847da89edbe8ef6fc3fb9368ad292a7
Makkohli <manish.kohli.ug21@nsut.ac.in>
2024-09-06 23:10:19 +0530

    added tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
-// // import { Stack } from "expo-router";
+import { Stack } from "expo-router";
 
-// export default function RootLayout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" options={{ 
-//           headerShown: false 
-//         }}  />
-//     </Stack>
-//   );
-// }
+export default function RootLayout() {
+  return (
+    <Stack>
+      {/* <Stack.Screen name="index" options={{ 
+          headerShown: false 
+        }}  /> */}
+        <Stack.Screen name="(tabs)" options={{ 
+          headerShown: false }} />
+    </Stack>
+  );
+}
