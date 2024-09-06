8fa75a1655fb0985eafadfcdcab3c6a09b243ac4
Manish Kohli <48186851+Makkohli@users.noreply.github.com>
2024-09-07 03:53:53 +0530

    Update _layout.jsx

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -11,7 +11,6 @@ export default function _layout() {
     <Tabs.Screen name='contact'/>
     <Tabs.Screen name='profile'/>
     <Tabs.Screen name='settings'/>
-    <Tabs.Screen name ='faq'/>
    </Tabs>
   )
 }
